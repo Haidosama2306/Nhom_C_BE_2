6445ad7095a543782974f24e7d3e78a649012795
Haidosama2306 <tranphanquangminh2306@gmail.com>
2024-03-25 14:20:46 +0700

    edit header

--- laravel/resources/views/client/index.blade.php
+++ laravel/resources/views/client/index.blade.php
@@ -1,85 +1,101 @@
 @extends('layouts.client.master')
-@section('title','Home')
+@section('title','- Trang Chủ')
 @section('content')
 
 <div class="container">
     <section>
-    <div class="row">
-        <div class="col-7">
-            <div class="news_1">
-                <div class="img"></div>
-                <h1><a href="#">news_1</a></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi itaque tempora voluptatum quod ut at sed recusandae id earum, sit labore odit magnam quas provident. Quis voluptates incidunt et velit, enim amet ex. Laboriosam, mollitia dolorum cupiditate tenetur totam voluptas soluta fuga consectetur blanditiis, quia, molestiae praesentium animi cum ab!</p>
-            </div>
-            <div class="row">
-            <div class="col-4">
-                <div class="news_2">
-                    <h4><a href="#">News_2</a></h4>
+        <div class="row">
+            <div class="col-7">
+                <div class="news_1">
                     <div class="img"></div>
+                    <a class="title" href="#">
+                        <h1>news_1</h1>
+                    </a>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi itaque tempora voluptatum quod ut
+                        at sed recusandae id earum, sit labore odit magnam quas provident. Quis voluptates incidunt et
+                        velit, enim amet ex. Laboriosam, mollitia dolorum cupiditate tenetur totam voluptas soluta fuga
+                        consectetur blanditiis, quia, molestiae praesentium animi cum ab!</p>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <div class="news_2">
+                            <a class="title" href="#">
+                                <h4>News_2</h4>
+                            </a>
+                            <div class="img"></div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="news_2">
+                            <a class="title" href="#">
+                                <h4>News_2</h4>
+                            </a>
+                            <div class="img"></div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="news_2">
+                            <a class="title" href="#">
+                                <h4>News_2</h4>
+                            </a>
+                            <div class="img"></div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="col-4">
-            <div class="news_2">
-                    <h4><a href="#">News_2</a></h4>
-                    <div class="img"></div>
-                </div>
-            </div>
-            <div class="col-4">
-            <div class="news_2">
-                    <h4><a href="#">News_2</a></h4>
-                    <div class="img"></div>
-                </div>
-            </div>
-        </div>
-        </div>
 
-        <div class="QC col-4">
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis molestias vero, explicabo, odio sequi necessitatibus, dolore harum tempore quas a consequuntur aliquam ab ipsam ullam dolor! Quae officiis voluptates maiores! Non numquam deserunt sed quisquam eum mollitia aliquid eaque, quae, ipsa iusto, animi officia sequi! Veritatis quas ipsum commodi. Similique iusto dolor deleniti numquam doloremque quos tenetur quae suscipit dicta animi nam blanditiis ipsum, nostrum architecto reprehenderit, nulla doloribus nemo quo odit iste iure provident? Illum, expedita! Alias quisquam provident distinctio maiores, molestiae assumenda laboriosam obcaecati dolor facere ipsum quibusdam neque ab soluta asperiores facilis unde. Molestiae deleniti ullam temporibus!</p>
+            <div class="QC col-4">
+
+            </div>
+            <div class="border-bottom"></div>
         </div>
-        <div class="border-bottom"></div>
-    </div>
     </section>
 
     <section>
         <div class="row">
             <div class="col-5">
                 <div class="news_3">
-                    <h4><a href="#">News_2</a></h4>
+                    <h4><a class="title" href="#">News_2</a></h4>
                     <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae quidem esse dolores veritatis tempore repellat.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
+                        quidem esse dolores veritatis tempore repellat.</p>
                 </div>
                 <div class="news_3">
-                    <h4><a href="#">News_2</a></h4>
+                    <h4><a class="title" href="#">News_2</a></h4>
                     <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae quidem esse dolores veritatis tempore repellat.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
+                        quidem esse dolores veritatis tempore repellat.</p>
                 </div>
                 <div class="news_3">
-                    <h4><a href="#">News_2</a></h4>
+                    <h4><a class="title" href="#">News_2</a></h4>
                     <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae quidem esse dolores veritatis tempore repellat.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
+                        quidem esse dolores veritatis tempore repellat.</p>
                 </div>
             </div>
             <div class="col-7">
                 <div class="row">
                     <div class="col-3">
-                        <h5><a href="#">News_2</a></h5>
+                        <h5><a class="title " style="border-bottom: 2px solid red;" href="#">Catalogues_children</a></h5>
                     </div>
                     <div class="col-3">
-                        <h5><a href="#">News_2</a></h5>
+                        <h5><a class="title" href="#">Catalogues_children</a></h5>
                     </div>
                     <div class="col-3">
-                        <h5><a href="#">News_2</a></h5>
+                        <h5><a class="title" href="#">Catalogues_children</a></h5>
                     </div>
                     <div class="col-3">
-                        <h5><a href="#">News_2</a></h5>
+                        <h5><a class="title" href="#">Catalogues_children</a></h5>
                     </div>
+                    <div class="border-bottom"></div>
                 </div>
                 <div class="row">
                     <div class="col-8">
                         <div class="news_4">
-                            <h4><a href="#">News_2</a></h4>
+                            <h4><a class="title" href="#">News_2</a></h4>
                             <div class="img"></div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iste qui ex quo minus fugiat odit, voluptate nemo soluta veritatis.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iste qui ex quo minus
+                                fugiat odit, voluptate nemo soluta veritatis.</p>
                         </div>
                     </div>
 
--- laravel/resources/views/layouts/client/master.blade.php
+++ laravel/resources/views/layouts/client/master.blade.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-@include('layouts.client._head')
+    @include('layouts.client._head')
 </head>
+
 <body>
-<div id="wrapper" class="wrap">
-<header>
-    <!-- headerTop -->
-    @include('layouts.client.headerTop')
+    <div id="wrapper" class="wrap">
+        <header>
+            <!-- headerTop -->
+            @include('layouts.client.headerTop')
 
-    <!-- headerNav -->
-    @include('layouts.client.headerNav')
-</header>
+            <!-- headerNav -->
+            @include('layouts.client.headerNav')
+        </header>
 
-<section>
-    <div class="container">
-        <!-- content -->
-        @yield('content')
-    </div>
-</section>
+        <div class="container">
+            <!-- content -->
+            @yield('content')
+        </div>
 
-<footer>
-    @include('layouts.client.footer')
-</footer>
+        <footer>
+            @include('layouts.client.footer')
+        </footer>
 
-<!-- javascript -->
-@include('layouts.client._script')
+        <!-- javascript -->
+        @include('layouts.client._script')
 </body>
+
 </html>
