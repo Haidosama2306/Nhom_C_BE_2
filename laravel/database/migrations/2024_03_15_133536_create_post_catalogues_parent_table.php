55f4a1b88454b6fdaea8a154106ef6fb20b53b32
vochuong57 <vochuong57@gmail.com>
2024-04-20 09:51:28 +0700

    Chương/1-login-logout

---
+++ laravel/database/migrations/2024_03_15_133536_create_post_catalogues_parent_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('post_catalogues_parent', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+	    $table->timestamp('deleted_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('post_catalogues_parent');
+    }
+};
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Backend\AuthController;
+use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Middleware\AuthenticateMiddleware;
+use App\Http\Middleware\LoginMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('admin',[AuthController::class, 'index'])->name('auth.admin')->middleware(LoginMiddleware::class);
+Route::post('login',[AuthController::class, 'login'])->name('auth.login');
+Route::get('logout',[AuthController::class, 'logout'])->name('auth.logout');
+
+Route::get('dashboard/index',[DashboardController::class, 'index'])->name('dashboard.index')->middleware(AuthenticateMiddleware::class);
+
+
