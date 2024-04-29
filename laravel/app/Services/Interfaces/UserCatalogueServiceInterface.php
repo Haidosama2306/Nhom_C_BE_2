a9119a0448732cc3988a8cf1cba45216a12683f4
vochuong57 <vochuong57@gmail.com>
2024-04-29 10:09:06 +0700

    Chương/4-d-user-catalogues

--- laravel/app/Services/Interfaces/UserCatalogueServiceInterface.php
+++ laravel/app/Services/Interfaces/UserCatalogueServiceInterface.php
@@ -11,4 +11,5 @@ interface UserCatalogueServiceInterface
     public function paginate($request);
     public function createUserCatalogue($request);
     public function updateUserCatalogue($id, $request);
+    public function deleteUserCatalogue($id);
 }
