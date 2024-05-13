8db10da537b34cfa9bb6d78e90c7de479768f8ec
TranMinh <tranphanquangminh2306@gmail.com>
2024-05-14 03:46:34 +0700

    add countrysnew, update latestnews

--- laravel/resources/views/client/index.blade.php
+++ laravel/resources/views/client/index.blade.php
@@ -3,65 +3,21 @@
 @section('content')
 
 <div class="container">
-    <section>
-       @include('client.countrysnews');
-    </section>
-
-    <section>
-        <div class="row">
-            <div class="col-5">
-                <div class="news_3">
-                    <h4><a class="title" href="#">News_2</a></h4>
-                    <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
-                        quidem esse dolores veritatis tempore repellat.</p>
-                </div>
-                <div class="news_3">
-                    <h4><a class="title" href="#">News_2</a></h4>
-                    <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
-                        quidem esse dolores veritatis tempore repellat.</p>
-                </div>
-                <div class="news_3">
-                    <h4><a class="title" href="#">News_2</a></h4>
-                    <div class="img"></div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni obcaecati, neque nisi at beatae
-                        quidem esse dolores veritatis tempore repellat.</p>
-                </div>
-            </div>
-            <div class="col-7">
-                <div class="row">
-                    <div class="col-3">
-                        <h5><a class="title " style="border-bottom: 2px solid red;" href="#">Catalogues_children</a></h5>
-                    </div>
-                    <div class="col-3">
-                        <h5><a class="title" href="#">Catalogues_children</a></h5>
-                    </div>
-                    <div class="col-3">
-                        <h5><a class="title" href="#">Catalogues_children</a></h5>
-                    </div>
-                    <div class="col-3">
-                        <h5><a class="title" href="#">Catalogues_children</a></h5>
-                    </div>
-                    <div class="border-bottom"></div>
-                </div>
-                <div class="row">
-                    <div class="col-8">
-                        <div class="news_4">
-                            <h4><a class="title" href="#">News_2</a></h4>
-                            <div class="img"></div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio iste qui ex quo minus
-                                fugiat odit, voluptate nemo soluta veritatis.</p>
-                        </div>
-                    </div>
-
-                    <div class="col-4">
-
-                    </div>
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-8">
+            <section>
+                @include('client.latestnews');
+            </section>
+            <section>
+                @include('client.countrysnews');
+            </section>
         </div>
-    </section>
 
+        <div class="col-4">
+            <section>
+
+            </section>
+        </div>
+    </div>
 </div>
 @endsection
