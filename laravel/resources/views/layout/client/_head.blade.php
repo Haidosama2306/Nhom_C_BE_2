36f966880a86210e27ad0e2abc912b83aabccf5b
TranMinh <tranphanquangminh2306@gmail.com>
2024-05-13 11:09:22 +0700

    add header, template homepage

---
+++ laravel/resources/views/layout/client/_head.blade.php
@@ -0,0 +1,15 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="icon" type="image/x-icon" href="{{ asset('icon.ico') }}" sizes="100x100">
+<title>Báo Mới @yield('title')</title>
+
+<!-- CSS -->
+<link rel="stylesheet" href="{{asset('css/client/style.css')}}">
+
+<!-- bootstrap  -->
+    <!-- headerTop -->
+
+    <!-- headerNav -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,500,500i,700,800i" rel="stylesheet">
---
+++ laravel/resources/views/layout/client/header.blade.php
@@ -0,0 +1,52 @@
+<div class="container">
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Báo Mới</a>
+
+            <div class="navbar-header headerTop">
+                <div class="row ">
+                    <div class="col-8">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link disabled" href="" tabindex="-1" aria-disabled="true">
+                                    <?php
+
+                            $dayNames = ['Thứ Hai', 'Thứ Ba', 'Thứ Tư', 'Thứ Năm', 'Thứ Sáu', 'Thứ Bảy', 'Chủ Nhật'];
+                            $dayOfWeek = strftime('%w');
+                            $dayOfWeekInVietnamese = $dayNames[$dayOfWeek - 1];
+
+                            $date = date('d/m/Y');
+                            echo $dayOfWeekInVietnamese .', '. $date; ?>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Tin Mới</a>
+                            </li>
+                            <li class="nav-item dropdown dmenu">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                   Tin Theo Khu Vực
+                                </a>
+                                <div class="dropdown-menu sm-menu">
+                                    <a class="dropdown-item" href="#">Hồ Chí Minh</a>
+                                    <a class="dropdown-item" href="#">Đà Nẵng</a>
+                                    <a class="dropdown-item" href="#">Hà Nội</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-4 search-form">
+                        <form class="navbar-form" role="search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Nhập từ khóa ..." name="keyword">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default" type="submit"><i
+                                            class="glyphicon glyphicon-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
