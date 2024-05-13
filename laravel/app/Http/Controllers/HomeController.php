df8a0d0fbd19fdf9d260f3f14ecf34caaa2489cd
TranMinh <tranphanquangminh2306@gmail.com>
2024-05-13 15:37:04 +0700

    add lastestnews

--- laravel/app/Http/Controllers/HomeController.php
+++ laravel/app/Http/Controllers/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends Controller
     public function index(){
         $data['post_catalogues_parent']=PostCatalogueParent::all();
         $data['post_catalogues_children']=PostCatalogueChildren::all();
-
+        $data['posts']=Post::all();
+        // $data['latest_posts']=Post::published()->orderBy('id', 'desc')->paginate(6);
         return view('client.index', $data);
     }
 }
